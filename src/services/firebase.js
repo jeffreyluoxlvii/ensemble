d23173d1c5ca77dbc8fa57b56b506d239571e463
jeffrey.luo <jeffrey.luo@duke.edu>
2020-12-05 14:56:29 -0800

    Created template

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase'
+import "firebase/auth";
+import "firebase/database";
+
 var config = {
     apiKey: "AIzaSyByuTcTjIv3I74eLaXfe-fP_1hEyj-kTgA",
     authDomain: "ensemble-7df30.firebaseapp.com",
-    projectId: "ensemble-7df30",
-    storageBucket: "ensemble-7df30.appspot.com",
-    messagingSenderId: "691614747142",
-    appId: "1:691614747142:web:8a802c614de6bb9904d187",
-    measurementId: "G-286P3S8VXP"
+    databaseURL: "https://ensemble-7df30-default-rtdb.firebaseio.com/",
   };
-var fire = firebase.initializeApp(config);
-export default fire;
+firebase.initializeApp(config);
+
+
+export const auth = firebase.auth;
+export const db = firebase.database();
