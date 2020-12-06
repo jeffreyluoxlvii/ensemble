84c79d0be02c2ec9a691a28abff08bb7da320ba4
jeffrey.luo <jeffrey.luo@duke.edu>
2020-12-05 19:45:11 -0800

    Added playing next song functionality

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -18,9 +18,11 @@ export default class Play extends Component {
       writeError: null,
       loadingQueue: true,
       loadingChats: true,
+      songIndex: 0,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.playNextSong = this.playNextSong.bind(this);
     this.myRef = React.createRef();
   }
 
@@ -41,6 +43,7 @@ export default class Play extends Component {
     } catch (error) {
       this.setState({ readError: error.message, loadingChats: false });
     }
+    // fetch the queue
     try {
       db.ref("queue").on("value", snapshot => {
         let queue = [];
@@ -48,8 +51,8 @@ export default class Play extends Component {
           queue.push(snap.val());
         });
         this.setState({ queue });
-        console.log(queue[0].videoId);
         this.setState({ loadingQueue: false });
+        console.log(queue);
       })
     } catch (error) {
       this.setState({ readError: error.message, loadingQueue: false });
@@ -62,6 +65,14 @@ export default class Play extends Component {
     });
   }
 
+  playNextSong() {
+    if(this.state.songIndex + 1 < this.state.queue.length) {
+      this.setState({
+        songIndex: this.state.songIndex + 1,
+      }, () => console.log(this.state.queue[this.state.songIndex]));
+    }
+  }
+
   async handleSubmit(event) {
     event.preventDefault();
     this.setState({ writeError: null });
@@ -72,7 +83,10 @@ export default class Play extends Component {
         timestamp: Date.now(),
         uid: this.state.user.uid
       });
-      addSong(await searchVideo(this.state.content));
+      console.log(this.state.content)
+      if (this.state.content.startsWith("-p ")) { // if content contains the play keyword
+        addSong(await searchVideo(this.state.content.substring(3)));
+      }
       this.setState({ content: '' });
       chatArea.scrollBy(0, chatArea.scrollHeight);
     } catch (error) {
@@ -82,48 +96,54 @@ export default class Play extends Component {
 
   formatTime(timestamp) {
     const d = new Date(timestamp);
-    const time = `${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
+    const time = `${d.getDate()}/${(d.getMonth() + 1)}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
     return time;
   }
 
   render() {
     return (
       <div>
-      <main className="container-fluid d-flex h-100 flex-column">
-        <Header/>
-        <div className="row main-container flex-fill">
-        <div className="col-9 main-instructions-column">
-          {/*
+        <main className="container-fluid d-flex h-100 flex-column">
+          <Header />
+          <div className="row main-container flex-fill">
+            <div className="col-9 main-instructions-column">
+              {/*
           To be filled out with music player content
           */}
-          {this.state.loadingQueue ? null : <ReactPlayer onReady={() => console.log("ready")} playing={true} url={`https://youtu.be/${this.state.queue[0].videoId}`} />}
-        </div>
-        <div className="col-3 main-command-column">
-          <div className="chat-area" ref={this.myRef}>
-            {/* loading indicator */}
-            {this.state.loadingChats ? <div className="spinner-border text-success" role="status">
-              <span className="sr-only">Loading...</span>
-            </div> : ""}
-            {/* chat area */}
-            {this.state.chats.map(chat => {
-              return <p key={chat.timestamp} className={"chat-bubble " + (this.state.user.uid === chat.uid ? "current-user" : "")}>
-                {chat.content}
-                <br />
-                <span className="chat-time float-right">{this.formatTime(chat.timestamp)}</span>
-              </p>
-            })}
+              {(this.state.loadingQueue || this.state.queue.length === 0) ? null :
+                <ReactPlayer
+                  playing={true} url={`https://youtu.be/${this.state.queue[this.state.songIndex].videoId}`}
+                />}
+              <button onClick={this.playNextSong}>
+                Next Song
+              </button>
+            </div>
+            <div className="col-3 main-command-column">
+              <div className="chat-area" ref={this.myRef}>
+                {/* loading indicator */}
+                {this.state.loadingChats ? <div className="spinner-border text-success" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div> : ""}
+                {/* chat area */}
+                {this.state.chats.map(chat => {
+                  return <p key={chat.timestamp} className={"chat-bubble " + (this.state.user.uid === chat.uid ? "current-user" : "")}>
+                    {chat.content}
+                    <br />
+                    <span className="chat-time float-right">{this.formatTime(chat.timestamp)}</span>
+                  </p>
+                })}
+              </div>
+              <form onSubmit={this.handleSubmit} className="mx-3">
+                <textarea className="border border-color w-100 rounded" name="content" rows="2" onChange={this.handleChange} value={this.state.content}></textarea>
+                {this.state.error ? <p className="text-danger">{this.state.error}</p> : null}
+                <button type="submit" className="btn btn-submit w-100 px-5 mt-4">Play</button>
+              </form>
+              <div className="py-5 mx-3">
+                Logged in as: <strong className="text-info">{this.state.user.email}</strong>
+              </div>
+            </div>
           </div>
-          <form onSubmit={this.handleSubmit} className="mx-3">
-            <textarea className="border border-color w-100 rounded" name="content" rows="2" onChange={this.handleChange} value={this.state.content}></textarea>
-            {this.state.error ? <p className="text-danger">{this.state.error}</p> : null}
-            <button type="submit" className="btn btn-submit w-100 px-5 mt-4">Play</button>
-          </form>
-          <div className="py-5 mx-3">
-            Login in as: <strong className="text-info">{this.state.user.email}</strong>
-          </div>
-        </div>
-        </div>
-      </main>
+        </main>
       </div>
     );
   }
